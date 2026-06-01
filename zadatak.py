11ecbdefc15467c89d44221f979ef72ed4750a7a
Vojin Zivanovic <vz200377@gmail.com>
2026-06-01 19:31:02 +0200

    dovrsiti zadatak

---
+++ zadatak.py
@@ -0,0 +1,16 @@
+# zadatak.py
+
+def izracunaj_sumu(lista):
+    # TODO: implementirati funkciju
+    pass
+
+
+def pronadji_maksimum(lista):
+    # TODO: implementirati funkciju
+    pass
+
+
+brojevi = [3, 5, 2, 8]
+
+print("Suma:", izracunaj_sumu(brojevi))
+print("Maksimum:", pronadji_maksimum(brojevi))
